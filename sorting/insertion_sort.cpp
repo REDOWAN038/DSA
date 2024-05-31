ef180c606e0964862dbd678dbef3fb31dc07f64c
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-05-31 16:57:48 +0600

    insertion sort

---
+++ sorting/insertion_sort.cpp
@@ -0,0 +1,39 @@
+// TC : O(N^2)
+// TC : O(N) for best case(already sorted)
+// SC : O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(int arr[], int n) {
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            j-=1;
+        }
+    }
+
+    cout << "After Using insertion sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using insertion Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    insertion_sort(arr, n);
+    return 0;
+}
