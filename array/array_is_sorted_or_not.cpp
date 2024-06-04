514d1b993d416b6ee808bfd64c7e45c48b5c4621
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-06-04 15:46:53 +0600

    array is sorted or not

---
+++ array/array_is_sorted_or_not.cpp
@@ -0,0 +1,26 @@
+// TC : O(n)
+// SC : O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isSorted(int arr[], int n) {
+  for (int i = 1; i < n; i++) {
+    if (arr[i] < arr[i - 1])
+      return false;
+  }
+
+  return true;
+}
+
+int main() {
+  int arr[] = {1, 2, 3, 4, 5};
+  int n = sizeof(arr)/sizeof(arr[0]);
+
+  if(isSorted(arr, n)){
+    printf("True\n");
+  }else{
+    printf("False\n");
+  }
+}
+
