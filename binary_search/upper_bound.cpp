5c396d75e9f579dbbd4be43cd7b9ca931db44ea4
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-06-28 21:20:26 +0600

    lower bound and upper bound

---
+++ binary_search/upper_bound.cpp
@@ -0,0 +1,30 @@
+// TC : O(logN)
+// SC : O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int>&arr, int target) {
+    int l = 0, h = arr.size(), res = arr.size();
+
+    while (l <= h) {
+        int m = (l + h) / 2;
+
+        if (arr[m] > target) {
+            res = m;
+            h -= 1;
+        } else {
+            l += 1;
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    vector<int> arr = {3, 5, 8, 9, 15, 19};
+    int target = 9;
+    cout << upperBound(arr, target) << endl;
+
+    return 0;
+}
