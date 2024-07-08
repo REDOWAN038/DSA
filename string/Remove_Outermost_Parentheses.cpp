feeb9f12b9e7fd666208a7fde8805d476bd78c43
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-07-09 00:18:23 +0600

    Remove Outermost Parentheses

---
+++ string/Remove_Outermost_Parentheses.cpp
@@ -0,0 +1,26 @@
+// TC : O(logN)
+// SC : O(1)
+// Leetcode 1021
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int cnt=0;
+        string res="";
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                if(cnt){
+                    res+=s[i];
+                }
+                cnt+=1;
+            }else{
+                cnt-=1;
+                if(cnt){
+                    res+=s[i];
+                }
+            }
+        }
+
+        return res;
+    }
+};
