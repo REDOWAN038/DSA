3f276ae9673f8f3157bcd06de1bb42ca66ae9969
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-07-18 20:55:47 +0600

    maximum nesting depth of paranthesis

---
+++ string/maximum_nesting_depth_of_paranthesis.cpp
@@ -0,0 +1,21 @@
+// TC : O(n)
+// SC : O(1)
+// Leetcode 1614
+
+class Solution {
+public:
+    int maxDepth(string s) {
+        int cnt=0,maxx=0;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                cnt+=1;
+                maxx = max(maxx, cnt);
+            }else if(s[i]==')'){
+                cnt-=1;
+            }
+        }
+
+        return maxx;
+    }
+};
